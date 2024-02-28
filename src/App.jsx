ff0bad1b255ebf664570ecbff394df5cb4a67cd2
EjikeOdoh <ejikeodoh2020@gmail.com>
2024-02-28 11:06:04 +0100

    add icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { FaBeer } from "react-icons/fa";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="flex items-center">
       <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
-    </>
+      <FaBeer />
+    </div>
   );
 }
 
